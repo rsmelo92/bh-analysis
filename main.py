00acdba293bd46cf78e9316ceb1364afd3d26646
Rafael Melo <rsmelo_@hotmail.com>
2020-10-14 00:42:27 -0300

    Add behavior_of_organisms

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from handlers import parse_file
 
+# Skinner books
+parse_file('behavior_of_organisms')
 parse_file('about_behaviorism')
-parse_file('shultz_and_shultz')
 parse_file('beyond_freedom_and_dignity')
+
+# Non-behaviorists books
+parse_file('shultz_and_shultz')
 
